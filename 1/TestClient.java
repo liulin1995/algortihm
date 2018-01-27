8d2efbca72e83f623089c89054d73b517fed1e85
liulin1995 <840395949@qq.com>
2018-01-27 12:33:06 +0800

    lecture1

---
+++ 1/TestClient.java
@@ -0,0 +1,21 @@
+
+
+public class TestClient{
+	
+	public static void main(string [] args)
+	{
+		int N = StdIn.readInt();
+		UF uf =new UF(N);
+		
+		while(!StdIn.isEmpty())
+		{
+			int p = StdIn.readInt();
+			int q = StdIn.readInt();
+			if(!uf.connected(p,q))
+			{
+				uf.union(p,q);
+				StdOut.println(p + " " + q);
+			}
+		}
+	}
+}
